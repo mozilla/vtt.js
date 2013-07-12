f4ef9784a12cd1895ebc50c2c7c90fa2e982da65
Andreas Gal <andreas.gal@gmail.com>
2013-07-12 19:05:11 +0200

    add a fake window constructor to the test harness

--- tests/run-tests.js
+++ tests/run-tests.js
@@ -9,6 +9,27 @@ var WebVTTParser = require("../").WebVTTParser,
     },
     print = console.log;
 
+// This implements a minimum fake window object constructor that is sufficient
+// to test constructing a DOM Tree for cue content.
+
+function FakeWindow() {
+  this.DocumentFragment = function () {
+    function appendChild(node) {
+      this.childNodes = this.childNodes || [];
+      this.childNodes.push(node);
+    }
+    this.createElement = function(tagName) {
+      return { tagName: tagName, appendChild: appendChild };
+    };
+    this.createTextNode = function(text) {
+      return { textContent: text };
+    };
+    this.appendChild = appendChild;
+  };
+  this.ProcessingInstruction = function () {
+  };
+};
+
 const FAIL = -1;
 
 function parse(callback, done) {
