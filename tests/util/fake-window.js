3ef91399141284de947500b719696be68606b5b5
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2013-07-15 14:17:59 -0400

    Fix tests failing on ProcessingInstruction nodes not having proper Node interface.

--- tests/util/fake-window.js
+++ tests/util/fake-window.js
@@ -1,20 +1,24 @@
 // This implements a minimum fake window object constructor that is sufficient
 // to test constructing a DOM Tree for cue content.
-module.exports = function FakeWindow() {
-  this.DocumentFragment = function () {
-    function appendChild(node) {
-      this.childNodes = this.childNodes || [];
-      this.childNodes.push(node);
-      node.parentNode = this;
-    }
-    this.createElement = function(tagName) {
-      return { tagName: tagName, appendChild: appendChild };
-    };
-    this.createTextNode = function(text) {
-      return { textContent: text };
-    };
-    this.appendChild = appendChild;
+
+function Node() {
+  function appendChild(node) {
+    this.childNodes = this.childNodes || [];
+    this.childNodes.push(node);
+    node.parentNode = this;
+  }
+  this.createElement = function(tagName) {
+    return { tagName: tagName, appendChild: appendChild };
   };
-  this.ProcessingInstruction = function () {
+  this.createTextNode = function(text) {
+    return { textContent: text };
+  };
+  this.appendChild = appendChild;
+}
+
+module.exports = function FakeWindow() {
+  this.DocumentFragment = Node;
+  this.ProcessingInstruction = function() {
+    return new Node();
   };
 };
