7e3d19df5932a93d0ced9e72aa57c2c114c1b5d5
Rick Eyre <rick.eyre@hotmail.com>
2013-07-17 16:33:33 -0400

    Update JSON handling to include region data as well.
    I've updated util.js and cue2json to generate and compare JSON that contains
    cue and region data.

--- bin/cue2json.js
+++ bin/cue2json.js
@@ -8,26 +8,6 @@ if (process.argv.length !== 3) {
 
 var filename = process.argv[2],
     util = require("../lib/util.js"),
-    vtt = util.parse(filename, true),
-    cues = vtt.cues;
+    vtt = util.parse(filename, true);
 
-function printCue(cue) {
-  cue.domTree = util.WebVTTParser.convertCueToDOMTree(new util.FakeWindow(), cue);
-  console.log(JSON.stringify(cue, util.filterJson, 2));
-}
-
-// Single cue
-if (cues.length === 1) {
-  printCue(cues[0]);
-}
-// Array of cues
-else {
-  console.log("[");
-  for (var i = 0; i < cues.length - 1; i++) {
-    printCue(cues[i]);
-    console.log(",");
-  }
-
-  printCue(cues[cues.length-1]);
-  console.log("]");
-}
+console.log(JSON.stringify(vtt, util.filterJson, 2));
