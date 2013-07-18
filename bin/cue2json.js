98ff25717a28d4184fddd4468ba2cd2da6681623
Rick Eyre <rick.eyre@hotmail.com>
2013-07-17 22:40:10 -0400

    Update cue2json.
    I've added functionality so that cue2json can print JSON to a file with
    the same name as the vtt passed or walk the a dir tree and rewrite JSON
    files that it finds have a matching vtt file.

--- bin/cue2json.js
+++ bin/cue2json.js
@@ -1,13 +1,52 @@
 #!/usr/bin/env node
 
-if (process.argv.length !== 3) {
-  console.error("Error: missing .vtt filename to process");
-  console.error("Usage: cue2json <filename>");
+if (process.argv.length < 3) {
+  console.error("Error: missing path to process\n" +
+                "Usage: cue2json [filename|dirname] [options]\n" +
+                "Options:\n" +
+                "-j: will parse the vtt file passed in and save it in a corresponding .json file");
   process.exit(1);
 }
 
-var filename = process.argv[2],
+var path = require("path"),
+    fs = require("fs"),
     util = require("../lib/util.js"),
-    vtt = util.parse(filename, true);
+    pathArg = process.argv[2],
+    options = process.argv.join();
 
-console.log(JSON.stringify(vtt, util.filterJson, 2));
+function getJson(vttFile) {
+  var vtt = util.parse(vttFile, true);
+  return JSON.stringify(vtt, util.filterJson, 2);
+}
+
+function flipName(fileName) {
+  if (fileName.match(/\.vtt$/))
+    return fileName.replace(/\.vtt$/, ".json");
+  else
+    return fileName.replace(/\.json$/, ".vtt");
+}
+
+function rewriteJson(dirName) {
+  var files = fs.readdirSync(dirName);
+  for (var i = 0; i < files.length; i++) {
+    var nextPath = path.join(dirName, files[i]);
+    if (fs.lstatSync(nextPath).isDirectory())
+      rewriteJson(nextPath);
+    else {
+      if (nextPath.match(/\.json$/)) {
+        var vttPath = flipName(nextPath);
+        if (fs.existsSync(vttPath))
+          fs.writeFileSync(nextPath, getJson(vttPath) + "\n");
+      }
+    }
+  }
+}
+
+if (fs.lstatSync(pathArg).isDirectory()) {
+  rewriteJson(pathArg);
+} else {
+  if (options.match(/-j/))
+    fs.writeFileSync(flipName(pathArg), getJson(pathArg) + "\n");
+  else
+    console.log(getJson(pathArg));
+}
