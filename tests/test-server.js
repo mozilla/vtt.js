621e0c99ec545d2b1c9c2e8d5424ff63e36aad87
Rick Eyre <rick.eyre@hotmail.com>
2013-11-06 15:07:34 -0500

    Small style fix in test-server.js.

--- tests/test-server.js
+++ tests/test-server.js
@@ -2,7 +2,7 @@ var express = require("express"),
     app = express(),
     util = require("../lib/util.js");
 
-app.all('*', function(req, res, next) {
+app.all("*", function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
