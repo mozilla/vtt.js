48431461801ad20055d14544184f8e71b878c8f8
Steve Heffernan <steve@zencoder.com>
2013-12-04 14:30:43 -0800

    Added Object.create shim for existing inheritance to support IE8.

--- vtt.js
+++ vtt.js
@@ -3,6 +3,20 @@
 
 (function(global) {
 
+  if (!Object.create) {
+    Object.create = (function(){
+      function F(){}
+
+      return function(o){
+        if (arguments.length != 1) {
+            throw new Error('Object.create implementation only accepts one parameter.');
+        }
+        F.prototype = o
+          return new F()
+      }
+    })()
+  }
+
   // Try to parse input as a time stamp.
   function parseTimeStamp(input) {
 
