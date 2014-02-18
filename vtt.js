10905a228dd7a61e2aef90ef414cae641ff7a68a
Rick Eyre <rick.eyre@hotmail.com>
2014-02-18 13:38:27 -0500

    Limit line cue setting to values in the -1000~1000 range.

--- vtt.js
+++ vtt.js
@@ -91,7 +91,8 @@
     // Accept a setting if its a valid (signed) integer.
     integer: function(k, v) {
       if (/^-?\d+$/.test(v)) { // integer
-        this.set(k, parseInt(v, 10));
+        // Only take values in the range of -1000 ~ 1000
+        this.set(k, Math.min(Math.max(parseInt(v, 10), -1000), 1000));
       }
     },
     // Accept a setting if its a valid percentage.
