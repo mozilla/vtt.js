efe299520bdd09a9fe2809cd0961bd0ad9432414
Rick Eyre <rick.eyre@hotmail.com>
2014-03-12 11:28:39 -0400

    Fix small linting issue.

--- vtt.js
+++ vtt.js
@@ -23,7 +23,7 @@
     function F(){}
 
     return function(o){
-      if (arguments.length != 1) {
+      if (arguments.length !== 1) {
         throw new Error('Object.create implementation only accepts one parameter.');
       }
       F.prototype = o;
