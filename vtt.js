mozilla/vtt.js

commit e55e9c2e8b5e972b324ae72b867c711fa59a5ae5

Uncatching an error to not hide issues

vtt.js
@@ -1352,6 +1352,8 @@
         // Enter BADWEBVTT state if header was not parsed correctly otherwise
         // another exception occurred so enter BADCUE state.
         self.state = self.state === "INITIAL" ? "BADWEBVTT" : "BADCUE";
+
+        throw e;
       }
       return this;
     },

commit 94af89e19329af6bd39910ccfa7145a54304273c

Don't throw errors if they are ParsingErrors.
Instead report them to the consumer of vtt.js.
This was a regression caused by commit:
e55e9c2e8b5e972b324ae72b867c711fa59a5ae5

vtt.js
@@ -1373,8 +1373,6 @@
         // Enter BADWEBVTT state if header was not parsed correctly otherwise
         // another exception occurred so enter BADCUE state.
         self.state = self.state === "INITIAL" ? "BADWEBVTT" : "BADCUE";
-
-        throw e;
       }
       return this;
     },