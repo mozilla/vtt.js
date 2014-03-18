1e219d02bba3781f6d116da1e053a053ed863b21
Rick Eyre <rick.eyre@hotmail.com>
2014-03-18 10:37:31 -0400

    Use Object.create if it is there.

--- vtt.js
+++ vtt.js
@@ -19,9 +19,8 @@
 
 (function(global) {
 
-  _objCreate = (function(){
+  _objCreate = Object.create || (function(){
     function F(){}
-
     return function(o){
       if (arguments.length !== 1) {
         throw new Error('Object.create implementation only accepts one parameter.');
