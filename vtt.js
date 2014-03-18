8a5b1a4c595aec1725bb87476b6ab8a21031446b
Rick Eyre <rick.eyre@hotmail.com>
2014-03-18 10:37:55 -0400

    Fix up Object.create shim and code.

--- vtt.js
+++ vtt.js
@@ -19,11 +19,11 @@
 
 (function(global) {
 
-  _objCreate = Object.create || (function(){
-    function F(){}
-    return function(o){
+  _objCreate = Object.create || (function() {
+    function F() {}
+    return function(o) {
       if (arguments.length !== 1) {
-        throw new Error('Object.create implementation only accepts one parameter.');
+        throw new Error('Object.create shim only accepts one parameter.');
       }
       F.prototype = o;
       return new F();
