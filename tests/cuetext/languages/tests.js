872df432f2ef6a08c10d706ae6275b5d558baa5e
Rick Eyre <rick.eyre@hotmail.com>
2014-03-26 16:00:28 -0400

    Enable all language tests.

--- tests/cuetext/languages/tests.js
+++ tests/cuetext/languages/tests.js
@@ -11,27 +11,27 @@ describe("cuetext/languages tests", function(){
     test.shutdown();
   });
 
-  it.skip("arabic.vtt", function(onDone){
+  it("arabic.vtt", function(onDone){
     test.jsonEqualAll("cuetext/languages/arabic.vtt", "cuetext/languages/arabic.json", onDone);
   });
 
-  it.skip("chinese.vtt", function(onDone){
+  it("chinese.vtt", function(onDone){
     test.jsonEqualAll("cuetext/languages/chinese.vtt", "cuetext/languages/chinese.json", onDone);
   });
 
-  it.skip("greek.vtt", function(onDone){
+  it("greek.vtt", function(onDone){
     test.jsonEqualAll("cuetext/languages/greek.vtt", "cuetext/languages/greek.json", onDone);
   });
 
-  it.skip("hebrew.vtt", function(onDone){
+  it("hebrew.vtt", function(onDone){
     test.jsonEqualAll("cuetext/languages/hebrew.vtt", "cuetext/languages/hebrew.json", onDone);
   });
 
-  it.skip("japanese.vtt", function(onDone){
+  it("japanese.vtt", function(onDone){
     test.jsonEqualAll("cuetext/languages/japanese.vtt", "cuetext/languages/japanese.json", onDone);
   });
 
-  it.skip("korean.vtt", function(onDone){
+  it("korean.vtt", function(onDone){
     test.jsonEqualAll("cuetext/languages/korean.vtt", "cuetext/languages/korean.json", onDone);
   });
 
@@ -39,56 +39,56 @@ describe("cuetext/languages tests", function(){
     test.jsonEqual("cuetext/languages/long_string_arabic.vtt", "cuetext/languages/long_string_arabic.json", onDone);
   });
 
-  it.skip("long_string_chinese.vtt", function(onDone){
-    test.jsonEqualAll("cuetext/languages/long_string_chinese.vtt", "cuetext/languages/long_string_chinese.json", onDone);
+  it("long_string_chinese.vtt", function(onDone){
+    test.jsonEqual("cuetext/languages/long_string_chinese.vtt", "cuetext/languages/long_string_chinese.json", onDone);
   });
 
-  it.skip("long_string_greek.vtt", function(onDone){
-    test.jsonEqualAll("cuetext/languages/long_string_greek.vtt", "cuetext/languages/long_string_greek.json", onDone);
+  it("long_string_greek.vtt", function(onDone){
+    test.jsonEqual("cuetext/languages/long_string_greek.vtt", "cuetext/languages/long_string_greek.json", onDone);
   });
 
-  it.skip("long_string_hebrew.vtt", function(onDone){
-    test.jsonEqualAll("cuetext/languages/long_string_hebrew.vtt", "cuetext/languages/long_string_hebrew.json", onDone);
+  it("long_string_hebrew.vtt", function(onDone){
+    test.jsonEqual("cuetext/languages/long_string_hebrew.vtt", "cuetext/languages/long_string_hebrew.json", onDone);
   });
 
-  it.skip("long_string_japanese.vtt", function(onDone){
-    test.jsonEqualAll("cuetext/languages/long_string_japanese.vtt", "cuetext/languages/long_string_japanese.json", onDone);
+  it("long_string_japanese.vtt", function(onDone){
+    test.jsonEqual("cuetext/languages/long_string_japanese.vtt", "cuetext/languages/long_string_japanese.json", onDone);
   });
 
-  it.skip("long_string_korean.vtt", function(onDone){
-    test.jsonEqualAll("cuetext/languages/long_string_korean.vtt", "cuetext/languages/long_string_korean.json", onDone);
+  it("long_string_korean.vtt", function(onDone){
+    test.jsonEqual("cuetext/languages/long_string_korean.vtt", "cuetext/languages/long_string_korean.json", onDone);
   });
 
-  it.skip("long_string_multiple_languages.vtt", function(onDone){
-    test.jsonEqualAll("cuetext/languages/long_string_multiple_languages.vtt", "cuetext/languages/long_string_multiple_languages.json", onDone);
+  it("long_string_multiple_languages.vtt", function(onDone){
+    test.jsonEqual("cuetext/languages/long_string_multiple_languages.vtt", "cuetext/languages/long_string_multiple_languages.json", onDone);
   });
 
-  it.skip("long_string_russian.vtt", function(onDone){
-    test.jsonEqualAll("cuetext/languages/long_string_russian.vtt", "cuetext/languages/long_string_russian.json", onDone);
+  it("long_string_russian.vtt", function(onDone){
+    test.jsonEqual("cuetext/languages/long_string_russian.vtt", "cuetext/languages/long_string_russian.json", onDone);
   });
 
-  it.skip("long_string_thai.vtt", function(onDone){
-    test.jsonEqualAll("cuetext/languages/long_string_thai.vtt", "cuetext/languages/long_string_thai.json", onDone);
+  it("long_string_thai.vtt", function(onDone){
+    test.jsonEqual("cuetext/languages/long_string_thai.vtt", "cuetext/languages/long_string_thai.json", onDone);
   });
 
-  it.skip("multiple_languages.vtt", function(onDone){
+  it("multiple_languages.vtt", function(onDone){
     test.jsonEqualAll("cuetext/languages/multiple_languages.vtt", "cuetext/languages/multiple_languages.json", onDone);
   });
 
-  it.skip("russian.vtt", function(onDone){
+  it("russian.vtt", function(onDone){
     test.jsonEqualAll("cuetext/languages/russian.vtt", "cuetext/languages/russian.json", onDone);
   });
 
-  it.skip("thai.vtt", function(onDone){
+  it("thai.vtt", function(onDone){
     test.jsonEqualAll("cuetext/languages/thai.vtt", "cuetext/languages/thai.json", onDone);
   });
 
-  it.skip("junk_characters.vtt", function(onDone){
+  it("junk_characters.vtt", function(onDone){
     test.jsonEqualAll("cuetext/languages/junk_characters.vtt", "cuetext/languages/junk_characters.json", onDone);
   });
 
-  it.skip("long_string_junk_characters.vtt", function(onDone){
-    test.jsonEqualAll("cuetext/languages/long_string_junk_characters.vtt", "cuetext/languages/long_string_junk_characters.json", onDone);
+  it("long_string_junk_characters.vtt", function(onDone){
+    test.jsonEqual("cuetext/languages/long_string_junk_characters.vtt", "cuetext/languages/long_string_junk_characters.json", onDone);
   });
 
 });
